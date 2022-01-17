45602872ad18766ee7889d055a34d36a1fdde85c
Andrea-creatore <sviluppatoreprogrammatore369@gmail.com>
2022-01-17 22:54:42 +0100

    ho provato ha sviluppare con una guida un contratto di staking per token, ho bisogno che mi spieghi delle righe di codice che dico su telegram. grazie.

--- dapp.sol
+++ dapp.sol
@@ -1,35 +1,91 @@
-//SPDX-License-Identifier: Unlicensed
+// SPDX-License-Identifier: GPL-3.0
+// sviluppo un meccanismo di stacking di token! (SPERO DI FARLO BENE, ALCUNE PAROLE E FRASI NON LE CAPISCO)
 
 pragma solidity 0.8.11;
 
-contract contratto{
+contract dappStaking{
 
-    struct strutture {
-       string name;
-       uint256 quantita;
-       address deposito;
+    using SafeMath for uint256;
+    
+    constructor(address _owner, uint256 _supply) public{
+       _mint(_owner, _supply);
     }
 
-    mapping(address => strutture[] ) public mappature ;
+    address[] internal stackHolders;
 
-
-    function deposit() public payable {
-       mappature[msg.sender].push(strutture("bnb", msg.value, address(0)));
-
+    function isStakeHolders(address _address) public view returns(bool, uint256){
+       for (uint256 s = 0; s < stakeholders.length; s += 1){
+          if (_address == stakeholders[s]) return (true, s);
+       }
+       return (false = 0);
     }
 
-    function prelevare() public {
-        mappature[msg.sender].pop(strutture("bnb", msg.value, address(0)));
+    function addStakeHolder(address _stackeHolder) public {
+       (bool _isStakeholder, ) = isStakeholder(_stakeholder);
+       if(!_isStakeholder) stakeholders.push(_stakeholder);
     }
 
-    string owner ="deployer";
-
-   constructor(owner _deployer) public {
-      owner = _deployer;
+   function removeStakeHolder(address _stackeHolder) public {
+      (bool _isStakeholder, uint256 s) = isStakeholder(_stakeholder);
+       if(_isStakeholder){
+           stakeholders[s] = stakeholders[stakeholders.length - 1];
+           stakeholders.pop();
    }
 
+   function stakeOf(address _stakeHolder) public view returns(uint256){
+     return stake[_stakeHolder];
+   }
+
+   function totalsStake() public view returns(uint256){
+      uint256 _totlasStake = 0;
+       for (uint256 s = 0; s < stakeholders.length; s += 1){
+           _totalStakes = _totalStakes.add(stakes[stakeholders[s]]);
+       }
+       return _totalStakes;
+   }
+
+   function createStake(uint256, _stake) public {
+       _burn(msg.sender, _stake);
+       if(stakes[msg.sender] == 0) addStakeholder(msg.sender);
+       stakes[msg.sender] = stakes[msg.sender].add(_stake);
+   }
+
+   function removeStake(uint256, _stake) public {
+      stakes[msg.sender] = stakes[msg.sender].sub(_stake);
+       if(stakes[msg.sender] == 0) removeStakeholder(msg.sender);
+       _mint(msg.sender, _stake);
+   }
+
+   mapping(address => uint256) internal rewards;
+
+   function rewards(address _stakeHolders) public view returns(uint256){
+       return rewards[_stakeHolder];
+   }
+   
+     function totalRewards() public view returns(uint256){
+         uint256 _totalRewards = 0;
+       for (uint256 s = 0; s < stakeholders.length; s += 1){
+           _totalRewards = _totalRewards.add(rewards[stakeholders[s]]);
+       }
+       return _totalRewards;
+     }
+
+     function calculateRewards(address _stakeHolder) public view returns(uint256){
+          return stakes[_stakeholder] / 100;
+     }
+
+     function distribuireRewards() public onlyOwer{
+         for (uint256 s = 0; s < stakeholders.length; s += 1){
+           address stakeholder = stakeholders[s];
+           uint256 reward = calculateReward(stakeholder);
+           rewards[stakeholder] = rewards[stakeholder].add(reward);
+       }
+     }
+
+     function whitDrowRewards() public {
+        uint256 reward = rewards[msg.sender];
+       rewards[msg.sender] = 0;
+       _mint(msg.sender, reward);
+     }
 
 }
-
-//funczione per prelevare cio che ho depositato(attento alla sicurezza)
-//constructor e metodo che rende owner il deployer(modifier)
